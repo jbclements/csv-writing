4710b3c75efb0d9e13c052385c7b10b03e383b98
John Clements <clements@racket-lang.org>
2018-11-08 20:53:29 -0800

    move most tests into separate file to test provide's

--- csv-writing/main.rkt
+++ csv-writing/main.rkt
@@ -2,9 +2,12 @@
 
 (provide table?
          display-table
+         display-table-row
          table->string
          table-row->string
-         table-cell->string)
+         table-cell->string
+         (struct-out csv-printing-params)
+         make-csv-printing-params)
 
 (require (only-in racket/list add-between)
          (only-in racket/format ~r))
@@ -218,24 +221,7 @@
                   (make-csv-printing-params
                    #:quotes-only-when-needed? #f))
                 "\"abcdef\"")
-  (check-equal? (table->string '(("abcdef"))
-                  #:printing-params
-                  (make-csv-printing-params
-                   #:quotes-only-when-needed? #f))
-                "\"abcdef\"")
-  (check-equal? (table-row->string '("abcdef")
-                  #:printing-params
-                  (make-csv-printing-params
-                   #:quotes-only-when-needed? #f))
-                "\"abcdef\"")
-  (check-equal? (table-cell->string "abcdef"
-                  (make-csv-printing-params
-                   #:quotes-only-when-needed? #f))
-                "\"abcdef\"")
-  (check-equal? (table-cell->string "abcdef"
-                  (make-csv-printing-params
-                   #:string-cell->string (λ (x) "ABC")))
-                "ABC")
+  
   
   (check-equal? (default-number-cell->string 234)
                 "234")
@@ -245,80 +231,8 @@
                 "1.333333")
   (check-equal? (default-number-cell->string -1242134.02)
                 "-1242134.02")
-
-  (check-equal? (table-row->string '(342 bc "def" #t))
-                "342,bc,def,TRUE")
-  (check-exn #px"string, number, boolean, or symbol"
-             (λ ()
-               (table-cell->string ''bc)))
-
-  (check-equal? (table->string '((name title)
-                                 ("joey" bottle-washer)
-                                 ("margo" sign-painter 34)))
-                "name,title
-joey,bottle-washer
-margo,sign-painter,34")
-
-  (check-equal? (table->string
-                 '((a b) (c d))
-                 #:printing-params
-                 (make-csv-printing-params
-                  #:table-cell->string (λ (str) "X")))
-                "X,X\nX,X")
-
-  (let ()
-    (define op (open-output-string))
-    (parameterize ([current-output-port op])
-      (display-table '((name title)
-                       ("joey" bottle-washer)
-                       ("margo" sign-painter 34))))
-    (check-equal? (get-output-string op)
-                  "name,title
-joey,bottle-washer
-margo,sign-painter,34\n"))
-
-  (let ()
-    (define op2 (open-output-string))
-    (display-table '((name title)
-                     ("joey" bottle-washer)
-                     ("margo" sign-painter 34 #f))
-                   op2)
-    (check-equal? (get-output-string op2)
-                  "name,title
-joey,bottle-washer
-margo,sign-painter,34,FALSE\n"))
-
-  (check-equal? (table? '()) #t)
-  (check-equal? (table? 14) #f)
-  (check-equal? (table? '((3 4) (4 5 6 "hamburger"))) #t)
-  (check-equal? (table? (list (list (void)))) #t)
-
-  ;; checks of error code
-  (check-exn #px"expected: table" (λ () (display-table 1234)))
-  (check-exn #px"expected: port" (λ () (display-table '((1234))
-                                                       4)))
-  (check-exn #px"expected: csv-printing-params"
-             (λ () (display-table '((1234))
-                                  #:printing-params 287)))
-  (check-exn #px"expected: list"
-             (λ () (display-table-row 999 (open-output-string))))
-  (check-exn #px"expected: output port"
-             (λ () (display-table-row '(999) 888)))
-  (check-exn #px"expected: csv-printing-params"
-             (λ () (display-table-row '(999) (open-output-string)
-                                      #:printing-params 287)))
-  (check-exn #px"expected: table"
-             (λ () (table->string 34)))
-  (check-exn #px"expected: csv-printing-params"
-             (λ () (table->string '((34))
-                                  #:printing-params 7)))
-
-  (check-exn #px"expected: list"
-             (λ () (table-row->string 7)))
-  (check-exn #px"expected: csv-printing-params"
-             (λ () (table-row->string '(7)
-                                      #:printing-params 8)))
-
+  
+ 
   (check-exn #px"expected: rational"
              (λ () (default-number-cell->string (sqrt -1))))
 
---
+++ csv-writing/tests.rkt
@@ -0,0 +1,100 @@
+#lang racket
+
+(require "main.rkt")
+
+(module+ test
+  (require rackunit)
+  
+  (check-equal? (table->string '(("abcdef"))
+                  #:printing-params
+                  (make-csv-printing-params
+                   #:quotes-only-when-needed? #f))
+                "\"abcdef\"")
+  (check-equal? (table-row->string '("abcdef")
+                  #:printing-params
+                  (make-csv-printing-params
+                   #:quotes-only-when-needed? #f))
+                "\"abcdef\"")
+  (check-equal? (table-cell->string "abcdef"
+                  (make-csv-printing-params
+                   #:quotes-only-when-needed? #f))
+                "\"abcdef\"")
+  (check-equal? (table-cell->string "abcdef"
+                  (make-csv-printing-params
+                   #:string-cell->string (λ (x) "ABC")))
+                "ABC")
+
+  (check-equal? (table-row->string '(342 bc "def" #t))
+                "342,bc,def,TRUE")
+  (check-exn #px"string, number, boolean, or symbol"
+             (λ ()
+               (table-cell->string ''bc)))
+
+  (check-equal? (table->string '((name title)
+                                 ("joey" bottle-washer)
+                                 ("margo" sign-painter 34)))
+                "name,title
+joey,bottle-washer
+margo,sign-painter,34")
+
+  (check-equal? (table->string
+                 '((a b) (c d))
+                 #:printing-params
+                 (make-csv-printing-params
+                  #:table-cell->string (λ (str) "X")))
+                "X,X\nX,X")
+
+  (let ()
+    (define op (open-output-string))
+    (parameterize ([current-output-port op])
+      (display-table '((name title)
+                       ("joey" bottle-washer)
+                       ("margo" sign-painter 34))))
+    (check-equal? (get-output-string op)
+                  "name,title
+joey,bottle-washer
+margo,sign-painter,34\n"))
+
+  (let ()
+    (define op2 (open-output-string))
+    (display-table '((name title)
+                     ("joey" bottle-washer)
+                     ("margo" sign-painter 34 #f))
+                   op2)
+    (check-equal? (get-output-string op2)
+                  "name,title
+joey,bottle-washer
+margo,sign-painter,34,FALSE\n"))
+
+  (check-equal? (table? '()) #t)
+  (check-equal? (table? 14) #f)
+  (check-equal? (table? '((3 4) (4 5 6 "hamburger"))) #t)
+  (check-equal? (table? (list (list (void)))) #t)
+
+  ;; checks of error code
+  (check-exn #px"expected: table" (λ () (display-table 1234)))
+  (check-exn #px"expected: port" (λ () (display-table '((1234))
+                                                       4)))
+  (check-exn #px"expected: csv-printing-params"
+             (λ () (display-table '((1234))
+                                  #:printing-params 287)))
+  (check-exn #px"expected: list"
+             (λ () (display-table-row 999 (open-output-string))))
+  (check-exn #px"expected: output port"
+             (λ () (display-table-row '(999) 888)))
+  (check-exn #px"expected: csv-printing-params"
+             (λ () (display-table-row '(999) (open-output-string)
+                                      #:printing-params 287)))
+  (check-exn #px"expected: table"
+             (λ () (table->string 34)))
+  (check-exn #px"expected: csv-printing-params"
+             (λ () (table->string '((34))
+                                  #:printing-params 7)))
+
+  (check-exn #px"expected: list"
+             (λ () (table-row->string 7)))
+  (check-exn #px"expected: csv-printing-params"
+             (λ () (table-row->string '(7)
+                                      #:printing-params 8)))
+
+  )
